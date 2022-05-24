fa63aeb48888c767228dfd0e245e30948c4aa722
venkatasrikanthp <venkatasrikanthp1@gmail.com>
2022-05-24 12:31:26 +0530

    Create OccurenceOfCharInString.java

---
+++ OccurenceOfCharInString.java
@@ -0,0 +1,27 @@
+//Print the number of each character occurenes in a given string
+import java.util.Map;
+import java.util.HashMap;
+class OccurenceOfCharInString {
+    static void characterCount(String name)
+    {
+        HashMap<Character, Integer> charCountMap
+            = new HashMap<Character, Integer>();
+        char[] nameArray = name.toCharArray();
+        for (char singleChar : nameArray) {
+            if (charCountMap.containsKey(singleChar)) {
+                charCountMap.put(singleChar, charCountMap.get(singleChar) + 1);
+            }
+            else {
+                charCountMap.put(singleChar, 1);
+            }
+        }
+        for (Map.Entry eachEntryInMap : charCountMap.entrySet()) {
+            System.out.println(eachEntryInMap.getKey() + ": " + eachEntryInMap.getValue());
+        }
+    }
+    public static void main(String[] args)
+    {
+        String name = "Sreekanth";
+        characterCount(name);
+    }
+}
